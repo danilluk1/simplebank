1acc90a773b7540caef0b1efb77f108925c1459a
danluki <danluki02@yandex.ru>
2023-02-23 12:50:25 +0500

    jwt token + unit tests

---
+++ token/paseto_maker.go
@@ -0,0 +1,18 @@
+package token
+
+import (
+	"fmt"
+	"github.com/o1egl/paseto"
+	"vendor/golang.org/x/crypto/chacha20poly1305"
+)
+
+type PasetoMaker struct {
+	paseto       *paseto.V2
+	symmetricKey []byte
+}
+
+func NewPasetoMaker(symmetricKey string) (Maker, error) {
+	if len(symmetricKey) != chacha20poly1305.KeySize {
+		return nil, fmt.Errorf("invalid key size: must be exactly %d characters", chacha20poly1305.KeySize)
+	}
+}
